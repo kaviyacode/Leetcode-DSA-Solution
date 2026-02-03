147f567220ce6ed18155e5e5174706e80d789f3b
Kaviya Sri B <165787960+kaviyacode@users.noreply.github.com>
2026-02-03 22:46:50 +0530

    Time: 4 ms (99.64%) | Memory: 46 MB (47.55%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int rev=0;
-        int temp=x;
-        if(x<0)
-        {
+        // Negative or ends with 0 but not 0 itself → not palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-        while(x!=0)
-        {
-            int ld=x%10;
-            rev=rev*10+ld;
-            x=x/10;
+
+        int rev = 0;
+        while (x > rev) {
+            int ld = x % 10;
+            rev = rev * 10 + ld;
+            x /= 10;
         }
-        if(temp==rev)
-        {
-            return true;
-        }
-    return false;
+
+        // If number has odd digits, rev/10 removes the middle digit
+        return x == rev || x == rev / 10;
     }
 }
